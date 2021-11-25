b1128d778b8d3aaf3f3e30dc801eadeecf04822a
ALPerrier <90027602+ALPerrier@users.noreply.github.com>
2021-11-25 09:13:32 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -110,4 +110,24 @@ def main():
          
 # Calls the main function
 if __name__ == "__main__":
+    
+    
+
+    pygame.init()
+    display=(600,600)
+    pygame.display.set_mode(display, pygame.DOUBLEBUF | pygame.OPENGL)
+    
+    # Sets the screen color (white)
+    gl.glClearColor(1, 1, 1, 1)
+    # Clears the buffers and sets DEPTH_TEST to remove hidden surfaces
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)                  
+    gl.glEnable(gl.GL_DEPTH_TEST)    
+    
+    # Placer ici l'utilisation de gluPerspective.
+
+    while True:
+        for event in pygame.event.get():
+        	if event.type == pygame.QUIT:
+        		pygame.quit()
+                exit()
     main()    
